e7aa583e6d0c6fd094dd0ac4a4a4f28c9e631bbd
hal0taso <yuya7719guitar@gmail.com>
2018-02-15 17:54:13 +0900

    [ADD] サンプル動かした

---
+++ src/1_introduction/Qconfig.py
@@ -0,0 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+APItoken = os.environ.get("APItoken")
+config = {'url': 'https://quantumexperience.ng.bluemix.net/api'}
+
+if 'APItoken' not in locals():
+    raise Exception('Please set up your access token. See Qconfig.py.')
---
+++ src/1_introduction/example01.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+# Check version of Python; supported > 3.5
+import sys
+if sys.version_info < (3, 5):
+    raise Exception('Please use Python version 3.5 or greater.')
+
+from qiskit import QuantumProgram
+import Qconfig
+
+
+## Creating Programs :
+# create your first QuantumProgram object instance
+qp = QuantumProgram()
+
+
+## Creating Registers :
+# create your first Quantum Register called "qr" with 2 qubits
+qr = qp.create_quantum_register('qr', 2)
+# create your first Classical Register called "cr" with 2 bits
+cr = qp.create_classical_register('cr', 2)
+
+
+## Creating Circuits
+# create your first Quantum Circuit called "qc" involving your Quantum Register "qr"
+# and your Classical Register "cr"
+qc = qp.create_circuit('Circuit', [qr], [cr])
+
+
+
+## another option for creating your QuantumProgram instance: 
+Q_SPECS = {
+    'circuits': [{
+        'name': 'Circuit',
+        'quantum_registers': [{
+            'name': 'qr',
+            'size': 4
+        }],
+        'classical_registers': [{
+            'name': 'cr',
+            'size': 4
+        }]
+    }],
+}
+
+qp = QuantumProgram(specs=Q_SPECS)
+
+
+
+## Get the components:
+# get the circuit by Name
+circuit = qp.get_circuit('Circuit')
+
+# get the Quantum Register by Name
+quantum_r = qp.get_quantum_register('qr')
+
+# get the Classical Register by Name
+classical_r = qp.get_classical_register('cr')
+
+## Building your Program: Add Gates to your Circuit !!
+# Pauli X gate to qubit 1 in the Quantum Register "qr"
+circuit.x(quantum_r[1])
+
+# Pauli Y gate to qubit 2 in the Quantum Register "qr"
+circuit.y(quantum_r[2])
+
+# Pauli Z gate to qubit 3 in the Quantum Register "qr"
+circuit.z(quantum_r[3])
+
+# CNOT (Controlled-NOT) gate from qubit 3 to qubit 2
+circuit.cx(quantum_r[3], quantum_r[2])
+
+# add a barrier to your circuit
+circuit.barrier()
+
+# H (Hadamard) gate to qubit 0 in the Quantum Register "qr"
+circuit.h(quantum_r[0])
+
+# S Phase gate to qubit 0
+circuit.s(quantum_r[0])
+
+# T Phase gate to qubit 1
+circuit.t(quantum_r[1])
+
+# identity gate to qubit
+circuit.iden(quantum_r[1])
+
+# first physical gete: u1(lambda) to qubit 0
+circuit.u1(0.3, quantum_r[0])
+
+# second physical gate: u2(phi, lambda) to qubit 1
+circuit.u2(0.3, 0.2, quantum_r[1])
+
+# third physical gate: u3(theta, phi, lambda) to qubit 2
+circuit.u3(0.3, 0.2, 0.1, quantum_r[2])
+
+# rotation around the x-axis to qubit 0
+circuit.rx(0.2, quantum_r[0])
+
+# rotation aroud the y-axis to qubit 1
+circuit.ry(0.2, quantum_r[1])
+
+# rotation aroud the z-acis to qubit 2
+circuit.rz(0.2, quantum_r[2])
+
+
+## Classical if, from qubit2 gate Z to classical bit 1
+# circuit.z(quantum_r[2]).c_if(classical_r, 0)
+
+# measure gate drom qubit 0 to classical bit 0
+print(circuit.measure(quantum_r[0], classical_r[0]),
+      circuit.measure(quantum_r[1], classical_r[1]),
+      circuit.measure(quantum_r[2], classical_r[2]),)
+
+QASM_source = qp.get_qasm('Circuit')
+
+print(QASM_source)
